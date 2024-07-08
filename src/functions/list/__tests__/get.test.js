4f7b58082ba5708e945cff3c16ff4a794d17052f
Enric Bisbe Gil <enricu@gmail.com>
2024-07-08 18:30:13 +0200

    fix test

--- src/functions/list/__tests__/get.test.js
+++ src/functions/list/__tests__/get.test.js
@@ -32,6 +32,6 @@ describe("GET List", () => {
     expect(response.statusCode).toBe(200);
 
     const body = JSON.parse(response.body);
-    expect(body.toString()).toBe(list.toString());
+    expect(body).toStrictEqual(JSON.parse(JSON.stringify(list)));
   });
 });
